666d9037787a3bd863d9c6aa00f512e04bba23d8
thNylHx <xiaohaoads@outlook.com>
2024-06-26 16:18:04 +0800

    Create foek1.sh

---
+++ Script/foek1.sh
@@ -0,0 +1,11 @@
+# 2024-06-10 21:15
+
+#!/bin/bash
+
+# 创建规则目录
+mkdir -p Tools/Ruleset/{Block,Media,Other}
+
+#--- Surge ---#
+
+# Other
+curl -L -o Tools-repo/Ruleset/Other/ChinaIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.list"
