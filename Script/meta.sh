608ea0fe633f44f1592b5ed49bebd8e95a15f5d1
thNylHx <xiaohaoads@outlook.com>
2024-06-27 20:25:18 +0800

    Update meta.sh

--- Script/meta.sh
+++ Script/meta.sh
@@ -7,14 +7,14 @@ mkdir -p Tools/Meta/Ruleset
 
 #--- Meta ---#
 
-# 
+# domain
 curl -L -o Tools-repo/Meta/Ruleset/China.list  "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.list"
 curl -L -o Tools-repo/Meta/Ruleset/GoogleIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.list"
 curl -L -o Tools-repo/Meta/Ruleset/YouTube.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.list"
 curl -L -o Tools-repo/Meta/Ruleset/Twitter.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/twitter.list"
 
 
-# IP
+# ipcidr
 curl -L -o Tools-repo/Meta/Ruleset/ChinaIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.list"
 curl -L -o Tools-repo/Meta/Ruleset/GoogleIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.list"
 curl -L -o Tools-repo/Meta/Ruleset/TwitterIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/twitter.list"
