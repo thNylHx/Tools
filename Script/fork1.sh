1a74cd09f82855e39cb14e8c355cc4c9bedb2949
thNylHx <xiaohaoads@outlook.com>
2024-06-27 21:19:04 +0800

    Update fork1.sh

--- Script/fork1.sh
+++ Script/fork1.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 # 创建规则目录
-mkdir -p Tools/Ruleset/{Block,Media,Other}
+mkdir -p Tools/Ruleset/{Block,Media,Other,Meta}
 
 #--- Surge ---#
 
@@ -11,3 +11,19 @@ mkdir -p Tools/Ruleset/{Block,Media,Other}
 curl -L -o Tools-repo/Ruleset/ChinaIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.list"
 curl -L -o Tools-repo/Ruleset/Other/GoogleIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.list"
 curl -L -o Tools-repo/Ruleset/Other/TwitterIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/twitter.list"
+
+
+#--- Meta ---#
+
+# domain
+curl -L -o Tools-repo/Ruleset/Meta/China.list  "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.list"
+curl -L -o Tools-repo/Ruleset/Meta/GoogleIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.list"
+curl -L -o Tools-repo/Ruleset/Meta/YouTube.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.list"
+curl -L -o Tools-repo/Ruleset/Meta/Twitter.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/twitter.list"
+curl -L -o Tools-repo/Ruleset/Meta/Netflix.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/netflix.list"
+
+# ipcidr
+curl -L -o Tools-repo/Ruleset/Meta/ChinaIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.list"
+curl -L -o Tools-repo/Ruleset/Meta/GoogleIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.list"
+curl -L -o Tools-repo/Ruleset/Meta/TwitterIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/twitter.list"
+curl -L -o Tools-repo/Ruleset/Meta/NetflixIP.list "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/netflix.list"
