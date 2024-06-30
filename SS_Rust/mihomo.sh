b5d454d55dbaeb6e72ffaddcc0b592510c5cb93c
thNylHx <xiaohaoads@outlook.com>
2024-06-30 16:00:31 +0800

    Update mihomo.sh

--- SS_Rust/mihomo.sh
+++ SS_Rust/mihomo.sh
@@ -21,14 +21,43 @@ VERSION=$(curl -s "https://api.github.com/repos/MetaCubeX/mihomo/releases?per_pa
 echo  "获取到的版本:${VERSION}"
 #ARCH
 
-curl -Lo mihomo.deb "https://github.com/SagerNet/mihomo/releases/download/Prerelease-Alpha/mihomo-linux-${ARCH}-compatible-alpha-${VERSION}.deb"
-echo "${VERSION}下载完成,开始安装"
+# 创建 mihomo 文件夹
+mkdir -p /root/mihomo
 
-sudo apt install ./mihomo.deb
+curl -Lo mihomo.deb "https://github.com/SagerNet/mihomo/releases/download/Prerelease-Alpha/mihomo-linux-${ARCH}-compatible-alpha-${VERSION}.gz"
 
-echo "安装完成,清理安装包"
+# 解压文件
+gzip -d mihomo-linux-${ARCH}-compatible-${VERSION}.gz
 
-rm mihomo.deb
+# 授权最高权限
+chmod 777 mihomo-linux-${ARCH}-compatible-${VERSION}
+
+# 重命名并移动到 /root/mihomo/
+mv mihomo-linux-${ARCH}-compatible-${VERSION} /root/mihomo/mihomo
+
+git clone https://github.com/metacubex/metacubexd.git -b gh-pages /root/mihomo/ui
+
+# 创建 systemd 配置文件
+cat << EOF > /etc/systemd/system/mihomo.service
+[Unit]
+Description=mihomo Daemon, Another Clash Kernel.
+After=network.target NetworkManager.service systemd-networkd.service iwd.service
+
+[Service]
+Type=simple
+LimitNPROC=500
+LimitNOFILE=1000000
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+Restart=always
+ExecStartPre=/usr/bin/sleep 1s
+ExecStart=/root/mihomo/mihomo -d /root/mihomo
+ExecReload=/bin/kill -HUP $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 
 systemctl daemon-reload
 systemctl restart mihomo
