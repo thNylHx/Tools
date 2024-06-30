5dff9f12010291f0034840de838bb046fd24b82a
thNylHx <xiaohaoads@outlook.com>
2024-06-30 19:11:38 +0800

    Update and rename 123.sh to mihomo-install.sh

--- SS_Rust/mihomo-install.sh
+++ SS_Rust/mihomo-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## bash <(curl -fsSL https://raw.githubusercontent.com/cooip-jm/About-openwrt/main/mihomo-deb-install.sh)
+## bash <(curl -fsSL https://raw.githubusercontent.com/thNylHx/Tools/main/SS_Rust/mihomo-install.sh)
 
 #!/bin/bash
 set -e -o pipefail
@@ -18,27 +18,33 @@ echo  "当前设备架构: ${ARCH_RAW}"
 
 VERSION=$(curl -L "https://github.com/MetaCubeX/mihomo/releases/download/Prerelease-Alpha/version.txt" || { echo "Failed to fetch version"; exit 1; })
 
-echo  "获取到的版本: ${VERSION}"
+echo  "获取到的最新版本: ${VERSION}"
 
+echo  "开始下载: ${VERSION}" 
 wget "https://github.com/MetaCubeX/mihomo/releases/download/Prerelease-Alpha/mihomo-linux-${ARCH}-compatible-${VERSION}.gz"
 echo "${VERSION}下载完成, 开始部署"
 
 echo "开始创建 mihomo 文件夹"
 # 创建 mihomo 文件夹
 mkdir -p /root/mihomo
-echo "开始加压 mihomo "
+
+echo "开始解压: mihomo "
 # 解压
-gzip -d mihomo-linux-amd64-compatible-${VERSION}.gz
-echo "开始授权 mihomo "
+gzip -d mihomo-linux-${ARCH}-compatible-${VERSION}.gz
+
+echo "开始授权最高权限 "
 # 授权
-chmod 777 mihomo-linux-amd64-compatible-${VERSION}
-echo "开始移动 mihomo "
+chmod 777 mihomo-linux-${ARCH}-compatible-${VERSION}
+
+echo "开始重名名为 mihomo 并移动到 /root/mihomo/ "
 # 移动
-mv mihomo-linux-amd64-compatible-${VERSION} /root/mihomo/mihomo
+mv mihomo-linux-${ARCH}-compatible-${VERSION} /root/mihomo/mihomo
+
 echo "开始安装 mihomo UI "
 # 安装UI
 git clone https://github.com/metacubex/metacubexd.git -b gh-pages /root/mihomo/ui
-echo "开始系统服务 "
+
+echo "开始安装系统服务 "
 # 安装服务
 cat << EOF > /etc/systemd/system/mihomo.service
 [Unit]
@@ -60,4 +66,5 @@ ExecReload=/bin/kill -HUP $MAINPID
 WantedBy=multi-user.target
 EOF
 
-echo "安装完成，上传你的config.ymal 文件 就可以使用"
+
+echo "恭喜你，已安装完成，上传你的config.ymal 文件，就可以使用"
