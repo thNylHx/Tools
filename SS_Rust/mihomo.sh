dab3a38ca2c1cfe8be4fffa2c4b46ed902cf3540
thNylHx <xiaohaoads@outlook.com>
2024-06-30 16:41:03 +0800

    Update mihomo.sh

--- SS_Rust/mihomo.sh
+++ SS_Rust/mihomo.sh
@@ -14,14 +14,15 @@ esac
 
 echo "当前设备架构: ${ARCH_RAW}"
 
-# 获取最新版本号
+# 获取最新版本
 VERSION=$(curl -s "https://api.github.com/repos/MetaCubeX/mihomo/releases?per_page=1&page=0" \
     | grep tag_name \
     | cut -d ":" -f2 \
+    | tr '[:upper:]' '[:lower:]' \
     | sed 's/\"//g;s/\,//g;s/\ //g;s/v//' \
-    | sed 's/Prerelease-Alpha //')
+    | sed 's/prerelease-alpha //')
 
-echo "获取到的版本号: ${VERSION}"
+echo "获取到的最新版本: ${VERSION}"
 
 # 创建 mihomo 文件夹
 mkdir -p /root/mihomo
