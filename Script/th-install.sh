b4ce86e78a0f3de65b87c000edef8759940804ae
Tools Add <xiaohaoads@outlook.com>
2024-08-13 21:07:04 +0800

    Update and rename quan-install.sh to th-install.sh

---
+++ Script/th-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## bash <(curl -fsSL )
+
+echo "================================="
+echo "欢迎使用一键脚本"
+echo "作者：thNylHx"
+echo "请保证科学上网已经开启"
+echo "安装过程中可以按ctrl+c强制退出"
+echo "================================="
+
+
+
+
+echo "================================="
+echo " 1、 安装 docker和 docker-compose"
+echo " 2、 安装 青龙 到宿主机"
+echo " 3、 安装 AdGuardHome"
+echo " 4、 安装 Trojan-go"
+echo " 5、 安装 VMESS"
+echo " 6、 安装 mihomo"
+echo " 0、 退出一键安装脚本"
+echo "================================="
+
+
