ffc9a6db5be49ccedaaa7a1e553ad9187ab67cd5
Tools Add <xiaohaoads@outlook.com>
2024-08-13 21:51:38 +0800

    Update thNylHx.sh

--- Script/thNylHx.sh
+++ Script/thNylHx.sh
@@ -122,7 +122,12 @@ while true; do
 
         9)
         echo "开始一键更换软件源（Debian 12）"
-        # 这里应该插入更换软件源的命令
+        cat << EOF > /etc/apt/sources.list
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
+        deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
+        EOF
         echo "软件源更换完成!"
         ;;
 
