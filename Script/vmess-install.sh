aea748e39ab0e451e5f0db42a96f08e5b0d8dda1
Tools Add <xiaohaoads@outlook.com>
2024-08-13 19:45:31 +0800

    Update vmess-install.sh

--- Script/vmess-install.sh
+++ Script/vmess-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-## bash <(curl -fsSL https://raw.githubusercontent.com/thNylHx/Tools/main/Script/vmess-install.sh)
-
-#!/bin/bash
 set -e -o pipefail
 
 ARCH_RAW=$(uname -m)
@@ -14,16 +10,27 @@ case "${ARCH_RAW}" in
     's390x')    ARCH='s390x';;
     *)          echo "Unsupported architecture: ${ARCH_RAW}"; exit 1;;
 esac
-echo  "当前设备架构: ${ARCH_RAW}"
+echo "当前设备架构: ${ARCH_RAW}"
 
 VERSION=$(curl -s "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" \
     | grep tag_name \
     | cut -d ":" -f2 \
     | sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
 
-echo  "获取到的最新版本: ${VERSION}"
+echo "获取到的最新版本: ${VERSION}"
 
-echo  "开始下载: mihomo" 
+echo "开始下载 v2ray-core"
 
-wget "https://github.com/v2fly/v2ray-core/releases/download/$VERSION/v2ray-linux-${ARCH_RAW}.zip"
-echo "mihomo 下载完成, 开始部署"
+# 注意这里使用 ${ARCH} 而不是 ${ARCH_RAW}
+wget "https://github.com/v2fly/v2ray-core/releases/download/v${VERSION}/v2ray-linux-${ARCH}.zip"
+
+echo "v2ray-core 下载完成, 开始部署"
+
+# 解压下载的 zip 文件
+unzip "v2ray-linux-${ARCH}.zip" -d /usr/local/bin/v2ray
+
+# 赋予可执行权限
+chmod +x /usr/local/bin/v2ray/v2ray
+chmod +x /usr/local/bin/v2ray/v2ctl
+
+echo "部署完成"
