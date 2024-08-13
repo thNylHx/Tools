503ef9db52d358b6c4ac52d344d6d83056bdf6b5
Tools Add <xiaohaoads@outlook.com>
2024-08-13 20:09:42 +0800

    Update vmess-install.sh

--- Script/vmess-install.sh
+++ Script/vmess-install.sh
@@ -4,6 +4,12 @@
 
 set -e -o pipefail
 
+echo "开始创建 V2ray 文件夹"
+mkdir -p /root/V2ray
+
+cd /root/V2ray
+echo "创建完成"
+
 ARCH_RAW=$(uname -m)
 case "${ARCH_RAW}" in
     'x86_64')    ARCH='64';;
@@ -24,7 +30,59 @@ echo "获取到的最新版本: ${VERSION}"
 
 echo "开始下载 v2ray-core"
 
-# 根据设备架构下载对应的版本
-wget "https://github.com/v2fly/v2ray-core/releases/download/v${VERSION}/v2ray-linux-${ARCH}.zip"
+# 根据设备架构下载对应的版本到指定文件夹
+wget -P /root/v2ray "https://github.com/v2fly/v2ray-core/releases/download/v${VERSION}/v2ray-linux-${ARCH}.zip"
 
 echo "v2ray-core 下载完成, 开始部署"
+
+
+echo "解压 V2ray"
+
+unzip v2ray-linux-64.zip && rm v2ray-linux-64.zip
+
+echo "配置 V2ray"
+cat << EOF > /etc/systemd/system/v2ray.service
+[Unit]
+Description=V2Ray Service
+Documentation=https://www.v2fly.org/
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/root/V2ray/v2ray run -config /root/V2ray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "服务端配置文件 vmess+tcp"
+cat << EOF > /root/V2ray/config.json
+{
+  "inbounds": [
+    {
+      "port": 443, 
+      "protocol": "vmess",    
+      "settings": {
+        "clients": [
+          {
+            "id": "af41686b-cb85-494a-a554-eeaa1514bca7",  
+            "alterId": 0
+          }
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",  
+      "settings": {}
+    }
+  ],
+  "vmess-aead": true
+}
+EOF
