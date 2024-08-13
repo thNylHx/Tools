4a96bc7ca3fe88048934db5ba3e9f99d78ce4a81
Tools Add <xiaohaoads@outlook.com>
2024-08-13 19:55:27 +0800

    Update vmess-install.sh

--- Script/vmess-install.sh
+++ Script/vmess-install.sh
@@ -6,8 +6,8 @@ set -e -o pipefail
 
 ARCH_RAW=$(uname -m)
 case "${ARCH_RAW}" in
-    'x86_64')    ARCH='x64';;
-    'x86' | 'i686' | 'i386')     ARCH='x86';;
+    'x86_64')    ARCH='64';;
+    'x86' | 'i686' | 'i386')     ARCH='86';;
     'aarch64' | 'arm64') ARCH='arm64';;
     'armv7l')   ARCH='armv7';;
     's390x')    ARCH='s390x';;
