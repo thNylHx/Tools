d1c003d00536a7657e1305963250d70c0617a456
Tools Add <xiaohaoads@outlook.com>
2024-08-13 19:16:22 +0800

    Create vmess-install.sh

---
+++ Script/vmess-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## bash <(curl -fsSL https://raw.githubusercontent.com/thNylHx/Tools/main/Install/mihomo-install.sh)
+
+#!/bin/bash
+set -e -o pipefail
+
+ARCH_RAW=$(uname -m)
+case "${ARCH_RAW}" in
+    'x86_64')    ARCH='amd64';;
+    'x86' | 'i686' | 'i386')     ARCH='386';;
+    'aarch64' | 'arm64') ARCH='arm64';;
+    'armv7l')   ARCH='armv7';;
+    's390x')    ARCH='s390x';;
+    *)          echo "Unsupported architecture: ${ARCH_RAW}"; exit 1;;
+esac
+echo  "当前设备架构: ${ARCH_RAW}"
+
+VERSION=$(curl -s "https://api.github.com/repos/v2fly/v2ray-core/releases/latest" \
+    | grep tag_name \
+    | cut -d ":" -f2 \
+    | sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+
+echo  "获取到的最新版本: ${VERSION}"
+
+echo  "开始下载: mihomo" 
+
+wget "https://github.com/v2fly/v2ray-core/releases/download/$VERSION/v2ray-linux-${ARCH_RAW}.zip"
+echo "mihomo 下载完成, 开始部署"
