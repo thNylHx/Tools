87564fc11ea553aba491aa75438a7a78335cdeec
thNylHx <173687855+thNylHx@users.noreply.github.com>
2025-03-14 16:36:25 +0800

    Update 2025-03-14 16:36:25

--- Script/Fork/mihomo.sh
+++ Script/Fork/mihomo.sh
@@ -1,55 +1,68 @@
-# 2024-10-31 18:40
-
 #!/bin/bash
+
+# 2025-03-14 17:00
 
 # 创建规则目录
 mkdir -p Tools/Ruleset/mihomo/{geoip,geosite}
 
-#--- mihomo ---#
-# geosite
-# ADS
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Ads_all.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-ads-all.mrs"
-# 本地/局域网
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Local.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/private.mrs"
-# 国内直连
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/China.mrs  "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.mrs"
-# AI
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Openai.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/openai.mrs"
-# PayPal
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/PayPal.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/paypal.mrs"
-# 苹果
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Apple.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/apple.mrs"
-# 海外代理
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Global.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/geolocation-!cn.mrs"
-# 谷歌平台
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Google.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/YouTube.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.mrs"
-# 游戏平台
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Steam.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/steam.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Epic.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/epicgames.mrs"
-# 社交平台
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Facebook.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/facebook.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Instagram.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/instagram.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Twitter.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/twitter.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Telegram.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/telegram.mrs"
-# 微软平台
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Line.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/line.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/GitHub.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/github.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/OneDrive.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/onedrive.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Microsoft.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/microsoft.mrs"
-# 影视媒体
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Tiktok.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/tiktok.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Bilibili.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/bilibili.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Disney.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/disney.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Netflix.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/netflix.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geosite/Spotify.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/spotify.mrs"
+# 定义下载函数
+download_file() {
+  local output_path="$1"
+  local url="$2"
+  curl -L -o "$output_path" "$url"
+}
 
+#--- mihomo mrs ---#
 
-# geoip
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Local.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/private.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/China.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Netflix.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/netflix.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Twitter.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/twitter.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Telegram.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/telegram.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Facebook.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/facebook.mrs"
-curl -L -o Tools-repo/Ruleset/mihomo/geoip/Google.mrs "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.mrs"
+# geosite 文件列表，格式：文件名|下载地址
+geosite_downloads=(
+  "Ads_all.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-ads-all.mrs"
+  "Local.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/private.mrs"
+  "China.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.mrs"
+  "Openai.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/openai.mrs"
+  "PayPal.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/paypal.mrs"
+  "Apple.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/apple.mrs"
+  "Global.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/geolocation-!cn.mrs"
+  "Google.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.mrs"
+  "YouTube.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.mrs"
+  "Steam.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/steam.mrs"
+  "Epic.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/epicgames.mrs"
+  "Facebook.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/facebook.mrs"
+  "Instagram.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/instagram.mrs"
+  "Twitter.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/twitter.mrs"
+  "telegram.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/telegram.mrs"
+  "Line.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/line.mrs"
+  "GitHub.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/github.mrs"
+  "OneDrive.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/onedrive.mrs"
+  "Microsoft.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/microsoft.mrs"
+  "tiktok.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/tiktok.mrs"
+  "Bilibili.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/bilibili.mrs"
+  "Disney.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/disney.mrs"
+  "Netflix.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/netflix.mrs"
+  "Spotify.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/spotify.mrs"
+)
+
+# 循环下载 geosite 文件
+for item in "${geosite_downloads[@]}"; do
+  filename="${item%%|*}"
+  url="${item#*|}"
+  download_file "Tools-repo/Ruleset/mihomo/geosite/${filename}" "$url"
+done
+
+# geoip 文件列表，格式：文件名|下载地址
+geoip_downloads=(
+  "Local.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/private.mrs"
+  "China.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.mrs"
+  "Netflix.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/netflix.mrs"
+  "Twitter.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/twitter.mrs"
+  "telegram.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/telegram.mrs"
+  "Facebook.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/facebook.mrs"
+  "Google.mrs|https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.mrs"
+)
+
+# 循环下载 geoip 文件
+for item in "${geoip_downloads[@]}"; do
+  filename="${item%%|*}"
+  url="${item#*|}"
+  download_file "Tools-repo/Ruleset/mihomo/geoip/${filename}" "$url"
+done
